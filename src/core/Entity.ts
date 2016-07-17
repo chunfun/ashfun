2787da15bab0051be8e70c70dc1fe68a8c93f8c6
jason <981602305@qq.com>
2016-07-17 17:26:42 +0800

    [modify] add newfile for ashfun project

---
+++ src/core/Entity.ts
@@ -0,0 +1,25 @@
+module ash{
+
+   /**
+    * Entity
+    * 实体类由组件组成。因此，它必须有一个组件的集合
+    */
+   class Entity {
+       private static nameCount:number = 0;
+       /**
+        * 实体名称
+        */
+       private _name: string;
+       
+       previous: Entity;
+       next: Entity;
+
+       constructor(name:string = "") {
+           if(name){
+               this._name = name;
+           }else{
+               this._name = "_entity"+(++Entity.nameCount);
+           }
+       }
+   }
+}
