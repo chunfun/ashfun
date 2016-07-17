8e29870af80d0397749176d83df52bebcbaf6742
jason <981602305@qq.com>
2016-07-17 23:29:33 +0800

    [modify] add ash core

---
+++ src/core/System.ts
@@ -0,0 +1,20 @@
+module ashfun{
+
+    /**
+     * System
+     */
+    class System {
+        /**
+         * manage the list of systems within the engine. The previous system in the list.
+         */
+        previous : System;
+        /**
+         * manage the list of systems within the engine. The next system in the list.
+         */
+        next : System;
+
+        constructor(parameters) {
+            
+        }
+    }
+}
